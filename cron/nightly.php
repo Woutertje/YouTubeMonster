4dcf6d7cbff3f0c710767b7bae3276358641e6c9
Rick van der Staaij <info@futureportal.com>
2014-02-23 17:35:46 +0100

    Fixed database flaw & fixed test twitter CRON

--- cron/nightly.php
+++ cron/nightly.php
@@ -1,21 +1,25 @@
 <?php
-	# setup
-	$t = strtotime('now') + microtime();
-	include('../core/config.php');
-	$GLOBALS['config']['classesroot'] = '../classes/';
-	include('../core/startup.php');
-	
-	# Globals
-	global $db;
-	
-	# Reset todays ratings
-	$db->query('
-		UPDATE `videos`
-		SET `scoretoday` = 0
-	');
-	$db->query('
-		TRUNCATE TABLE `rates`
-	');
-	
-	# Finish
-	echo 'Done.';
+
+# setup
+$t = strtotime('now') + microtime();
+include('../core/config.php');
+$GLOBALS['config']['root'] = '../';
+include('../core/startup.php');
+
+# Make sure only admins can run this file
+onlyAdmin();
+
+# Globals
+global $db;
+
+# Reset todays ratings
+$db->query('
+    UPDATE `videos`
+    SET `scoretoday` = 0
+');
+$db->query('
+    TRUNCATE TABLE `rates`
+');
+
+# Finish
+echo 'Done.';
